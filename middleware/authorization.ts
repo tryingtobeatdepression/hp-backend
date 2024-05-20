d588bff055fb9547c2fc6fa6c62100ddcd798a5d
tryingtobeatdepression <mazeek.ol98@gmail.com>
2024-05-20 13:51:31 +0300

    Auth(#2) Fix

--- middleware/authorization.ts
+++ middleware/authorization.ts
@@ -2,10 +2,11 @@ import { NextFunction, Request, Response } from "express";
 import { UserRoles } from "../mongo/models/user.schema";
 import { AppError } from "../modules/common/errors";
 
-export const isRole = (role: number) => {
+export const isRole = (roles: number[]) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        if (req?.user?.role !== role)
-            return next(new AppError(`${UserRoles[role]}`, 403))
+        const role = req?.user?.role
+        if (!role || !roles.includes(role))
+            return next(new AppError(`${UserRoles[role!]}`, 403))
         next()
     }
 }
--- modules/common/routes-handler.ts
+++ modules/common/routes-handler.ts
@@ -2,9 +2,11 @@ import { Express } from "express"
 import * as userRouter from '../user/routes'
 import * as authRouter from '../auth/routes'
 import * as experienceRouter from "../experience/routes";
+import * as projectRouter from "../project/routes";
 
 export default function (app: Express) {
     app.use('/api/users/', userRouter.router)
     app.use('/api/auth/', authRouter.router)
     app.use('/api/experiences',experienceRouter.router)
+    app.use('/api/projects',projectRouter.router)
 }
