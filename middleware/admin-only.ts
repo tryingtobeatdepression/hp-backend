32840167c305a9132c39fbf9427614c88d5b3e8b
tryingtobeatdepression <mazeek.ol98@gmail.com>
2024-05-21 14:16:24 +0300

    Fix(#10)

--- middleware/admin-only.ts
+++ middleware/admin-only.ts
@@ -6,8 +6,6 @@ import { ErrorMessages } from "../enums/errors.enum";
 export const adminOnly = async (req: Request, res: Response, next: any) => {
     const id = req.params.id;
     const doc = await userRepo.findById(id)
-    if(doc?.role! === 'admin') console.log('Admin Record Being Edited')
-    if(req?.user?.role === 'visitor') console.log("Visitor Trying To Edit")
     if (doc?.role! === 'admin' && req?.user?.role === 'visitor')
         return next(new AppError(ErrorMessages.ADMIN_ONLY, 403))
     next()
