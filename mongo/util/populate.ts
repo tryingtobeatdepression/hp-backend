adfe7e4d99f0b073d2bb5b40e0f27139e0d06163
tryingtobeatdepression <mazeek.ol98@gmail.com>
2024-08-02 20:37:41 +0300

    Populate(#1)

---
+++ mongo/util/populate.ts
@@ -0,0 +1,34 @@
+import { FilterQuery, Model, Schema } from "mongoose";
+
+const getAllPopulatePaths = (schema: Schema) => {
+    const paths: string[] = [];
+    schema.eachPath((path, schemaType) => {
+        if (schemaType.options && schemaType.options.ref) {
+            paths.push(path);
+        }
+    });
+    return paths;
+};
+
+export const populateAll = 
+async (model: Model<any>, filter?: FilterQuery<any>, propertiesToExclude = [
+    '_id', 'password', 'updatedAt', 'createdAt', 'refreshToken'
+]) => {
+    const schemaPaths = getAllPopulatePaths(model.schema);
+    const selectStr = propertiesToExclude.map(prop => `-${prop}`).join(' ');
+    const populateOptions = schemaPaths.map(path => ({
+        path,
+        select: selectStr,
+    }));
+
+    let query = model.find(filter!);
+    populateOptions.forEach(option => {
+        query = query.populate(option);
+    });
+
+    return await query.exec();
+}
+
+
+
+
