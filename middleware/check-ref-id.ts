84b886bc546f77d4b77dc19e3fe0741978e48d58
tryingtobeatdepression <mazeek.ol98@gmail.com>
2024-08-03 12:52:34 +0300

    Fix(#113)

--- middleware/check-ref-id.ts
+++ middleware/check-ref-id.ts
@@ -5,6 +5,8 @@ import { AppError } from "../modules/common/errors";
 // TESTED ✅
 export const checkRefId = (repo: CrudRepository<any>, key: string) => {
     return async (req: Request, res: Response, next: NextFunction) => {
+        if(!req.body[key])
+            next()
         if(! await repo.findById(req.body[key]))
             return next(new AppError('ObjectId not found', 404))
         next()
