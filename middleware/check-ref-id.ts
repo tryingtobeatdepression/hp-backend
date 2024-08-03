35d8932b298a7c7a45a999a8cbd93d56f52f6965
tryingtobeatdepression <mazeek.ol98@gmail.com>
2024-08-03 13:27:48 +0300

    Fix(#114)

--- middleware/check-ref-id.ts
+++ middleware/check-ref-id.ts
@@ -3,9 +3,9 @@ import CrudRepository from "../mongo/repositories/crud.repo";
 import { AppError } from "../modules/common/errors";
 
 // TESTED ✅
-export const checkRefId = (repo: CrudRepository<any>, key: string) => {
+export const checkRefId = (repo: CrudRepository<any>, key: string, required: boolean = true) => {
     return async (req: Request, res: Response, next: NextFunction) => {
-        if(!req.body[key])
+        if(!required)
             next()
         if(! await repo.findById(req.body[key]))
             return next(new AppError('ObjectId not found', 404))
--- modules/user-exp-booking/routes.ts
+++ modules/user-exp-booking/routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express'
 import * as controller from './controller'
+import { checkRefId } from '../../middleware/check-ref-id'
+import { userRepo } from '../../mongo/repositories/user.repo'
+import { organizationExperienceRepository } from '../organization-experience/repository'
 
 export const router: Router = Router()
 
 router.route('/')
     .get(controller.list)
-    .post(controller.create)
+    .post(
+        checkRefId(userRepo, "user"),
+        checkRefId(organizationExperienceRepository, "orgExperience"),
+        controller.create
+    )
 
 router.route('/:id')
     .get(controller.findOne)
