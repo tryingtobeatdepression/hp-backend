a986f9d991835cc286670a29c6dd5c6aa8f83319
tryingtobeatdepression <mazeek.ol98@gmail.com>
2024-08-16 16:33:28 +0300

    Cors #1

--- app.ts
+++ app.ts
@@ -2,9 +2,14 @@ import express, { Express,} from 'express'
 import dbConfig from './mongo/config/db.config'
 import routesHandler from './modules/common/routes-handler'
 import globalError from './modules/common/errors'
+import cors from 'cors'
 
 const app: Express = express()
 app.use(express.json())
+app.use(cors({
+    origin: "*",
+    allowedHeaders: "*",
+}))
 
 declare global {
     namespace Express {
