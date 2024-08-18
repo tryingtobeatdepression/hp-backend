19849b24d2ede50b543fd7bcd2d544c431cde990
tryingtobeatdepression <mazeek.ol98@gmail.com>
2024-08-18 19:57:54 +0300

    Fix #207

--- modules/experience/model.ts
+++ modules/experience/model.ts
@@ -73,7 +73,6 @@ const schema = new Schema<IExperience>(
                 type: Number,
                 min: 1,
                 max: 5,
-                required: true,
             }
         }],
         includes: [
